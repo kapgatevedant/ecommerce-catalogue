78f50a899a4d4d7f5ccf4bf38e5a6c64794e198b
Vedant <kapgatevu123@gmail.com>
2024-10-01 00:10:37 +0530

    first commit

---
+++ products.js
@@ -0,0 +1,60 @@
+const products = [
+    {
+        "id": 1,
+        "name":" WALTHER PPQ M2 SEMI-AUTO",
+        "price": 271.99,
+        "image": "images/1.png",
+        "description": "Caliber .22 LR - Finish Color Black - Barrel Length 4 - Trigger Pull 4.85 lbs - Trigger Travel 0.16 - Capacity 12 Rds - Overall Length 7.1 - Height 5.3 - Width 1.3 - Safety 2 Auto - Weight (mag empty) 20.8 oz"
+    },
+    {
+        "id": 2,
+        "name":" ATN Blaze Trek 319 Thermal Monocular",
+        "price": 950.00,
+        "image": "images/2.png",
+        "description": "The ATN™ Blaze Trek 319 Thermal Monocular puts the cutting-edge advantage of thermal imaging in your pocket. Perfect for your next nighttime hunt, this thermal monocular has an advanced thermal sensor that produces a resolution of 384 x 288 (50 Hz) and an AMOLED display with a resolution of 800 x 600 for extra image clarity. This version comes equipped with 2-16X magnification, and its advanced thermal sensor has a detection range out to 1,083 yds., a recognition range of 208 yds., and an identification range of 104 yds. Its diopter adjustment wheel is located at the far end of the monocular and is quick and easy when focusing. With the click of a button, you can select from 5 different color pallets: White Hot, Black Hot, Rainbow, Iron Red, and Alarm."
+    },
+    {
+        "id": 3,
+        "name":" Smith & Wesson Volunteer XV",
+        "price": 599.99,
+        "image": "images/3.png",
+        "description": "Introducing the Smith & Wesson Volunteer Series! Built on greatness! S&W's rifles are chambered in 5.56 NATO and configured for a wide variety of recreational, sport shooting and professional applications! These rifles offer; premium stocks, grips, handguards, triggers, sights, upgraded 5R rifling and ambidextrous features. The S&W Volunteer Series rifles are purpose built and are everything you'll need right out of the box!"
+    },
+    {
+        "id": 4,
+        "name":" SMITH & WESSON M&P 9 Shield plus",
+        "price": 449.00,
+        "image": "images/4.png",
+        "description": "The M&P Shield Plus features is extremely thin and lightweight which can be comfortably carried all day. It features the M&P's patented take-down lever and sear deactivation systems all for disassembly without pulling the trigger. Its flat face trigger is for consistent trigger finger placement which allows for more accurate and repeatable shooting. Enhanced grip texture allows the user to manage recoil, keep a firm grip and delivers comfort when concealing. The Shield Plus also features an optimal 18-degree grip angle for natural point of aim and Armornite durable corrosion resistant finish. This Model comes with an Optic-Cut Slide and No Thumb Safety."
+    },
+    {
+        "id": 5,
+        "name":" SPRINGFIELD ARMORY XD 3-inch DEFENDER HIGH CAP",
+        "price": 345.99,
+        "image": "images/5.png",
+        "description": "The Springfield Armory XD 3-inch Sub-Compact offers the ergonomics and reliability characteristic of the full-size XD line, and its 3-inch hammer forged steel barrel and compact frame make it optimal for concealed carry. With its X-Tension magazine the semi-automatic XD 3-inch Sub-Compact sports a 16+1 9mm capacity, or 13+1 with its flush-fitting magazine, and 12+1 and 9+1 in 40 S&W, respectively. Its loaded chamber indicator, striker status indicator, grip safety, and Ultra Safety Assurance Action Trigger System make the XD 3-inch Sub-Compact a safer gun to use. Additional features include its durable polymer frame, Melonite-coated slide and barrel, and three dot steel sights."
+    },
+    {
+        "id": 6,
+        "name":" Sig Sauer Cross Bolt-Action Centerfire Rifle",
+        "price": 1599.99,
+        "image": "images/6.png",
+        "description": "Take your precision shooting to the next level with the Sig Sauer® Cross™ Bolt-Action Centerfire Rifle. Designed and built from the ground up with the input of hunters, military snipers and elite long-range shooters, the Cross rifle is specifically designed to meet the demands of precision long-range shooters and extreme backcountry hunters. Featuring a folding precision-style stock, this precision rifle boasts a 2-stage, match-grade trigger with a short, crisp pull. The oversized and textured bolt handle offers intuitive, smooth manipulation for rapid operation."
+    },
+    {
+        "id": 7,
+        "name":" Wicked Ridge M-370 Crossbow Package with ACUdraw",
+        "price": 499.97,
+        "image": "images/7.png",
+        "description": "The Wicked Ridge® M-370™ Crossbow Package with ACUdraw tips the scales at just 5.8 lbs., making it one of the lightest crossbows on the market! Pairing the lightweight design with a narrow 9.5-inch width when cocked, this hunting crossbow is ideal for anyone hunting in tight spaces or putting the miles on their boots in search of a wall-hanger! Zipping bolts downrange at speeds up to 370 fps, the M-370 is plenty capable, while the ACUdraw cocking system ensures drawing is safe, quiet, and easy. Topped with a Multi-Line scope for pinpoint precision. Pre-sighted at 20 yards, the Wicked Ridge M-370 Crossbow Package with ACUdraw is ready to shoot after simple, 1-bolt assembly."
+    },
+    {
+        "id": 8,
+        "name":" Cabela's Alaskan Guide Series 119 Special Fixed-Blade Knife by Buck Knives",
+        "price": 199.99,
+        "image": "images/8.png",
+        "description": "The Cabela's Alaskan Guide® Series 119 Special Fixed-Blade Knife by Buck Knives utilizes a ruged and robust clip-point blade design, which excels for general camp craft, outdoor survival, or fielding dressing big game. The blade is made of S30V steel, which has proven to hold an edge better than virtually any stainless steel on the market. A durable Cerakote® coating enhances the blade's corrosion resistance."
+    }
+    
+];
+export default products;
